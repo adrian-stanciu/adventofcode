000c5fb979c45a8647425b4f250d97dc0639e318
Adrian Stanciu <adrian.stanciu.pub@gmail.com>
2020-01-25 11:15:51 +0200

    add template hasher for vector

--- common/hasher.h
+++ common/hasher.h
@@ -15,5 +15,17 @@ namespace {
             return h;
         }
     };
+
+    template <typename T>
+    struct VectorHasher {
+        auto operator()(const std::vector<T>& v) const
+        {
+            // based on boost's hash_combine()
+            size_t h = 0;
+            for (auto i : v)
+                h ^= std::hash<T>{}(i) + 0x9e3779b9 + (h << 6) + (h >> 2);
+            return h;
+        }
+    };
 }
 
