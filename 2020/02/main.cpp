60edf60828c9f93424b666cf5aa7e391b3fb226f
Adrian Stanciu <adrian.stanciu.pub@gmail.com>
2020-12-02 07:11:41 +0200

    2020: add 02

---
+++ 2020/02/main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Policy {
+    long a;
+    long b;
+    char c;
+};
+
+struct Password {
+    Policy pol;
+    string pas;
+};
+
+int main()
+{
+    static const regex pw_regex{"([1-9][0-9]*)-([1-9][0-9]*) ([a-zA-Z]): (.*)"};
+
+    vector<Password> passwords;
+
+    string line;
+    while (getline(cin, line)) {
+        smatch matched;
+        regex_match(line, matched, pw_regex);
+
+        Policy pol{
+            strtol(matched[1].str().data(), nullptr, 10),
+            strtol(matched[2].str().data(), nullptr, 10),
+            matched[3].str()[0]
+        };
+        Password pw{move(pol), matched[4].str()};
+
+        passwords.push_back(move(pw));
+    }
+
+    cout << accumulate(begin(passwords), end(passwords), 0l, [] (auto acc, const auto& pw) {
+        auto cnt = count_if(begin(pw.pas), end(pw.pas), [&] (auto c) {
+            return c == pw.pol.c;
+        });
+        return acc + (cnt >= pw.pol.a && cnt <= pw.pol.b);
+    }) << '\n';
+
+    cout << accumulate(begin(passwords), end(passwords), 0l, [] (auto acc, const auto& pw) {
+        return acc + ((pw.pas[pw.pol.a - 1] == pw.pol.c) ^ (pw.pas[pw.pol.b - 1] == pw.pol.c));
+    }) << '\n';
+
+    return 0;
+}
+
