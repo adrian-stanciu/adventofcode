feb98bc7d5a1b0d9312da2c10dc721344d229d54
Adrian Stanciu <adrian.stanciu.pub@gmail.com>
2020-12-15 07:25:34 +0200

    2020: add 15

---
+++ 2020/15/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+auto solve(const vector<int>& v, size_t n)
+{
+    vector<int> last_pos(n, 0);
+    for (size_t i = 0; i < v.size() - 1; ++i)
+        last_pos[v[i]] = i + 1;
+
+    auto prev = v.back();
+
+    for (size_t i = v.size(); i < n; ++i) {
+        auto next = last_pos[prev] ? i - last_pos[prev] : 0;
+        last_pos[prev] = i;
+        prev = next;
+    }
+
+    return prev;
+}
+
+int main()
+{
+    vector<int> v;
+
+    string line;
+    while (getline(cin, line, ','))
+        v.push_back(strtol(line.data(), nullptr, 10));
+
+    cout << solve(v, 2020) << '\n';
+    cout << solve(v, 30000000) << '\n';
+
+    return 0;
+}
+
