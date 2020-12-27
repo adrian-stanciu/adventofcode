02c6c08f023e394e51de42d9745e137c44ad7be1
Adrian Stanciu <adrian.stanciu.pub@gmail.com>
2020-12-27 12:03:16 +0200

    2018/22: implement simpler A* solution

--- 2018/22/main.cpp
+++ 2018/22/main.cpp
@@ -66,216 +66,137 @@ auto sum_all_types(const TypeMap& t_map, int x_t, int y_t)
     return s;
 }
 
-enum Region {
+enum class Region {
     Rocky,
     Wet,
     Narrow,
 };
 
-struct Dist {
-    long to;
-    long cg;
-    long no;
-
-    long score;
+enum class Tool {
+    Torch,
+    ClimbingGear,
+    None,
 };
 
-bool operator==(const Dist& d1, const Dist& d2)
+bool is_compatible(Tool t, Region r)
 {
-    return d1.to == d2.to && d1.cg == d2.cg && d1.no == d2.no &&
-        d1.score == d2.score;
+    return (t == Tool::Torch && (r == Region::Rocky || r == Region::Narrow)) ||
+           (t == Tool::ClimbingGear && (r == Region::Rocky || r == Region::Wet)) ||
+           (t == Tool::None && (r == Region::Wet || r == Region::Narrow));
 }
 
-void init_dist(Dist& next_d, int next_pos_type,
-    const Dist& curr_d, int curr_pos_type)
+struct State {
+    int x;
+    int y;
+    Tool t;
+    long cost;
+    long score;
+
+    State(int x, int y, Tool t, long cost, long cost2target)
+    : x(x)
+    , y(y)
+    , t(t)
+    , cost(cost)
+    , score(cost + cost2target)
+    {}
+};
+
+bool operator==(const State& s1, const State& s2)
 {
-    if (curr_pos_type == Region::Rocky) {
-        if (next_pos_type == Region::Rocky) {
-            next_d.to = curr_d.to + 1;
-            next_d.cg = curr_d.cg + 1;
-            next_d.no = Inf;
-        } else if (next_pos_type == Region::Wet) {
-            next_d.to = Inf;
-            next_d.cg = curr_d.cg + 1;
-            next_d.no = curr_d.cg + 1 + ChangeCost;
-        } else if (next_pos_type == Region::Narrow) {
-            next_d.to = curr_d.to + 1;
-            next_d.cg = Inf;
-            next_d.no = curr_d.to + 1 + ChangeCost;
-        }
-    } else if (curr_pos_type == Region::Wet) {
-        if (next_pos_type == Region::Rocky) {
-            next_d.to = curr_d.cg + 1 + ChangeCost;
-            next_d.cg = curr_d.cg + 1;
-            next_d.no = Inf;
-        } else if (next_pos_type == Region::Wet) {
-            next_d.to = Inf;
-            next_d.cg = curr_d.cg + 1;
-            next_d.no = curr_d.no + 1;
-        } else if (next_pos_type == Region::Narrow) {
-            next_d.to = curr_d.no + 1 + ChangeCost;
-            next_d.cg = Inf;
-            next_d.no = curr_d.no + 1;
-        }
-    } else if (curr_pos_type == Region::Narrow) {
-        if (next_pos_type == Region::Rocky) {
-            next_d.to = curr_d.to + 1;
-            next_d.cg = curr_d.to + 1 + ChangeCost;
-            next_d.no = Inf;
-        } else if (next_pos_type == Region::Wet) {
-            next_d.to = Inf;
-            next_d.cg = curr_d.no + 1 + ChangeCost;
-            next_d.no = curr_d.no + 1;
-        } else if (next_pos_type == Region::Narrow) {
-            next_d.to = curr_d.to + 1;
-            next_d.cg = Inf;
-            next_d.no = curr_d.no + 1;
-        }
+    return s1.x == s2.x && s1.y == s2.y && s1.t == s2.t;
+}
+
+bool operator<(const State& s1, const State& s2)
+{
+    if (s1.score == s2.score) {
+        if (s1.cost == s2.cost)
+            return std::tie(s1.x, s1.y, s1.t) < std::tie(s2.x, s2.y, s2.t);
+        else
+            return s1.cost < s2.cost;
+    } else {
+        return s1.score < s2.score;
     }
 }
 
-void update_dist(Dist& next_d, int next_pos_type,
-    const Dist& curr_d, int curr_pos_type)
-{
-    auto to = next_d.to;
-    auto cg = next_d.cg;
-    auto no = next_d.no;
-
-    if (curr_pos_type == Region::Rocky) {
-        if (next_pos_type == Region::Rocky) {
-            to = std::min(to, curr_d.to + 1);
-            cg = std::min(cg, curr_d.cg + 1);
-        } else if (next_pos_type == Region::Wet) {
-            cg = std::min(cg, curr_d.cg + 1);
-            no = std::min(no, cg + ChangeCost);
-        } else if (next_pos_type == Region::Narrow) {
-            to = std::min(to, curr_d.to + 1);
-            no = std::min(no, to + ChangeCost);
-        }
-    } else if (curr_pos_type == Region::Wet) {
-        if (next_pos_type == Region::Rocky) {
-            cg = std::min(cg, curr_d.cg + 1);
-            to = std::min(to, cg + ChangeCost);
-        } else if (next_pos_type == Region::Wet) {
-            cg = std::min(cg, curr_d.cg + 1);
-            no = std::min(no, curr_d.no + 1);
-        } else if (next_pos_type == Region::Narrow) {
-            no = std::min(no, curr_d.no + 1);
-            to = std::min(to, no + ChangeCost);
-        }
-    } else if (curr_pos_type == Region::Narrow) {
-        if (next_pos_type == Region::Rocky) {
-            to = std::min(to, curr_d.to + 1);
-            cg = std::min(cg, to + ChangeCost);
-        } else if (next_pos_type == Region::Wet) {
-            no = std::min(no, curr_d.no + 1);
-            cg = std::min(cg, no + ChangeCost);
-        } else if (next_pos_type == Region::Narrow) {
-            to = std::min(to, curr_d.to + 1);
-            no = std::min(no, curr_d.no + 1);
-        }
+struct StateHasher {
+    auto operator()(const State& s) const
+    {
+        // based on boost's hash_combine()
+        size_t h = 0;
+        h ^= std::hash<decltype(s.x)>{}(s.x) + 0x9e3779b9 + (h << 6) + (h >> 2);
+        h ^= std::hash<decltype(s.y)>{}(s.y) + 0x9e3779b9 + (h << 6) + (h >> 2);
+        h ^= std::hash<decltype(s.t)>{}(s.t) + 0x9e3779b9 + (h << 6) + (h >> 2);
+        return h;
     }
+};
 
-    next_d.to = to;
-    next_d.cg = cg;
-    next_d.no = no;
-}
-
-auto compute_dist(const TypeMap& t_map, int x_t, int y_t, int limit)
+auto compute_cost(const TypeMap& t_map, int x_t, int y_t, int limit)
 {
-    const std::pair<int, int> neighbours[] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-    auto dist = [x_t, y_t] (int x, int y) {
-        return abs(x_t - x) + abs(y_t -y);
+    auto cost2target = [&] (auto x, auto y, auto t) {
+        return abs(x_t - x) + abs(y_t - y) + (t == Tool::Torch ? 0 : ChangeCost);
     };
 
-    auto update_score = [&dist] (auto& d, auto x, auto y) {
-        if (d.to == Inf)
-            d.score = std::min(d.cg, d.no) + dist(x, y) + ChangeCost;
-        else
-            d.score = d.to + dist(x, y);
+    auto next_states = [&] (const auto& s) {
+        static const std::pair<int, int> neigh[] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        static const Tool tools[] = {Tool::Torch, Tool::ClimbingGear, Tool::None};
+
+        std::vector<State> states;
+
+        for (auto [dx, dy] : neigh) {
+            auto next_x = s.x + dx;
+            if (next_x < 0 || next_x > limit)
+                continue;
+            auto next_y = s.y + dy;
+            if (next_y < 0 || next_y > limit)
+                continue;
+
+            if (is_compatible(s.t, static_cast<Region>(t_map[next_y][next_x])))
+                states.emplace_back(next_x, next_y, s.t,
+                    s.cost + 1, cost2target(next_x, next_y, s.t));
+        }
+
+        for (auto t : tools) {
+            if (t == s.t)
+                continue;
+
+            if (is_compatible(t, static_cast<Region>(t_map[s.y][s.x])))
+                states.emplace_back(s.x, s.y, t,
+                    s.cost + ChangeCost, cost2target(s.x, s.y, t));
+        }
+
+        return states;
     };
 
-    std::unordered_map<std::pair<int, int>, Dist, PairHasher<int>> d_map;
+    std::set<State> open_set;
+    std::unordered_set<State, StateHasher> closed_set;
 
-    auto point_cmp = [&d_map] (const auto& p1, const auto& p2) {
-        auto dist1 = d_map[p1];
-        auto dist2 = d_map[p2];
-
-        if (dist1.score == dist2.score)
-            return p1 < p2;
-        else
-            return dist1.score < dist2.score;
-    };
-
-    std::set<std::pair<int, int>, decltype(point_cmp)> open_set(point_cmp);
-    std::unordered_set<std::pair<int, int>, PairHasher<int>> closed_set;
-
-    d_map[std::make_pair(0, 0)] = {0, ChangeCost, Inf, dist(0, 0)};
-    open_set.emplace(0, 0);
+    open_set.emplace(0, 0, Tool::Torch, 0, cost2target(0, 0, Tool::Torch));
 
     while (!open_set.empty()) {
-        auto curr = *open_set.begin();
+        auto s = *open_set.begin();
         open_set.erase(open_set.begin());
 
-        if (curr.first == x_t && curr.second == y_t)
-            break;
+        if (s.x == x_t && s.y == y_t && s.t == Tool::Torch)
+            return s.cost;
 
-        closed_set.emplace(curr.first, curr.second);
+        closed_set.emplace(s);
 
-        const auto& curr_d = d_map[curr];
-        auto curr_pos_type = t_map[curr.second][curr.first];
-
-        for (const auto& [neigh_x, neigh_y] : neighbours) {
-            auto next_x = curr.first + neigh_x;
-            auto next_y = curr.second + neigh_y;
-
-            if (next_x < 0 || next_y < 0)
-                continue;
-            if (next_x > limit || next_y > limit)
-                continue;
-
-            auto next = std::make_pair(next_x, next_y);
-            auto next_pos_type = t_map[next_y][next_x];
-
+        for (const auto& next : next_states(s))
             if (closed_set.find(next) == closed_set.end()) {
                 auto it = open_set.find(next);
                 if (it == open_set.end()) {
-                    auto& next_d = d_map[next];
-
-                    init_dist(next_d, next_pos_type, curr_d, curr_pos_type);
-                    update_score(next_d, next_x, next_y);
-
-                    open_set.emplace(next_x, next_y);
+                    open_set.emplace(next);
                 } else {
-                    open_set.erase(it);
-
-                    auto& next_d = d_map[next];
-
-                    update_dist(next_d, next_pos_type, curr_d, curr_pos_type);
-                    update_score(next_d, next_x, next_y);
-
-                    open_set.emplace(std::move(next));
+                    if (it->score > next.score ||
+                        (it->score == next.score && it->cost > next.cost)) {
+                        open_set.erase(it);
+                        open_set.emplace(next);
+                    }
                 }
-            } else {
-                const auto& old_next_d = d_map[next];
-                auto new_next_d = d_map[next];
-
-                update_dist(new_next_d, next_pos_type, curr_d, curr_pos_type);
-                update_score(new_next_d, next_x, next_y);
-
-                if ((new_next_d.score > old_next_d.score) ||
-                    (new_next_d == old_next_d))
-                    continue;
-
-                d_map[next] = std::move(new_next_d);
-
-                open_set.emplace(std::move(next));
             }
-        }
     }
 
-    return d_map[std::make_pair(x_t, y_t)].to;
+    return Inf;
 }
 
 int main()
@@ -294,7 +215,7 @@ int main()
     auto t_map = build_type_map(depth, x_target, y_target, limit);
 
     std::cout << sum_all_types(t_map, x_target, y_target) << "\n";
-    std::cout << compute_dist(t_map, x_target, y_target, limit) << "\n";
+    std::cout << compute_cost(t_map, x_target, y_target, limit) << "\n";
 
     return 0;
 }
