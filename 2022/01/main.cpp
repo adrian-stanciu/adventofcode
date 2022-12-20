45b628af253e2b382ee85cbbf4b595c0dd82a304
Adrian Stanciu <adrian.stanciu.pub@gmail.com>
2022-12-20 09:06:21 +0200

    2022/01: fix bugs: * fix move_backward()'s parameters * update max sums with last group's sum

--- 2022/01/main.cpp
+++ 2022/01/main.cpp
@@ -7,20 +7,25 @@ int main()
     auto sum{0l};
     array max3sums{0l, 0l, 0l};
 
+    auto update_max3sums = [&](auto sum) {
+        for (auto i = 0; i < ssize(max3sums); ++i)
+            if (sum >= max3sums[i]) {
+                move_backward(begin(max3sums) + i, end(max3sums) - 1, end(max3sums));
+                max3sums[i] = sum;
+                break;
+            }
+    };
+
     string line;
     while (getline(cin, line)) {
         if (line.empty()) {
-            for (auto i = 0; i < ssize(max3sums); ++i)
-                if (sum >= max3sums[i]) {
-                    move_backward(begin(max3sums) + i, end(max3sums) - 1, begin(max3sums) + i + 1);
-                    max3sums[i] = sum;
-                    break;
-                }
+            update_max3sums(sum);
             sum = 0;
         } else {
             sum += strtol(line.data(), nullptr, 10);
         }
     }
+    update_max3sums(sum);
 
     cout << max3sums[0] << '\n';
     cout << accumulate(begin(max3sums), end(max3sums), 0l) << '\n';
