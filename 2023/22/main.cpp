d6732acbde7d0cbef299fd21c88f2eba9f901fdf
Adrian Stanciu <adrian.stanciu.pub@gmail.com>
2023-12-23 16:41:19 +0200

    2023: add 22

---
+++ 2023/22/main.cpp
@@ -0,0 +1,154 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+auto str2num(string_view sv)
+{
+    auto n = 0;
+    from_chars(sv.data(), sv.data() + sv.size(), n);
+    return n;
+}
+
+auto split(string_view sv, char sep)
+{
+    while (sv.front() == sep)
+        sv.remove_prefix(1);
+    while (sv.back() == sep)
+        sv.remove_suffix(1);
+
+    vector<string_view> split_sv;
+
+    while (true)
+        if (auto to = sv.find_first_of(sep); to == sv.npos) {
+            split_sv.push_back(sv);
+            break;
+        } else {
+            split_sv.push_back(sv.substr(0, to));
+            sv.remove_prefix(to + 1);
+        }
+
+    return split_sv;
+}
+
+struct Point {
+    int z;
+    int x;
+    int y;
+
+    auto operator<=>(const Point&) const = default;
+};
+
+struct Seg {
+    Point from;
+    Point to;
+    int idx;
+
+    Seg(Point from, Point to, int idx) : from{from}, to{to}, idx{idx} {}
+
+    auto operator<=>(const Seg&) const = default;
+
+    [[nodiscard]] bool is_on(const Seg& other) const
+    {
+        auto intersect = [](auto lhs_from, auto lhs_to, auto rhs_from, auto rhs_to) {
+            return max(lhs_from, rhs_from) <= min(lhs_to, rhs_to);
+        };
+
+        return intersect(from.x, to.x, other.from.x, other.to.x) &&
+            intersect(from.y, to.y, other.from.y, other.to.y);
+    }
+};
+
+auto count_dependant(vector<set<int>> below_vec, const vector<set<int>>& above_vec, int u)
+{
+    queue<int> q;
+    q.emplace(u);
+
+    auto cnt = 0;
+
+    while (!q.empty()) {
+        auto u = q.front();
+        q.pop();
+
+        for (auto v : above_vec[u]) {
+            below_vec[v].erase(u);
+
+            if (below_vec[v].empty()) {
+                ++cnt;
+                q.emplace(v);
+            }
+        }
+    }
+
+    return cnt;
+}
+
+void solve(int n, vector<Seg>& v)
+{
+    sort(begin(v), end(v));
+
+    vector<set<int>> below_vec(n);
+    vector<set<int>> above_vec(n);
+
+    for (auto i = 0; i < n; ++i) {
+        auto top_z = 0;
+
+        for (auto j = i - 1; j >= 0; --j) {
+            if (v[j].to.z < top_z)
+                break;
+
+            if (v[i].is_on(v[j])) {
+                below_vec[v[i].idx].insert(v[j].idx);
+                above_vec[v[j].idx].insert(v[i].idx);
+
+                top_z = v[j].to.z;
+            }
+        }
+
+        v[i].to.z = top_z + 1 + (v[i].to.z - v[i].from.z);
+        v[i].from.z = top_z + 1;
+
+        sort(begin(v), begin(v) + i + 1, [](const auto& lhs, const auto& rhs) {
+            return tie(lhs.to, lhs.from, lhs.idx) < tie(rhs.to, rhs.from, rhs.idx);
+        });
+    }
+
+    vector<bool> can_be_removed(n, true);
+    for (auto& below : below_vec)
+        if (ssize(below) == 1)
+            can_be_removed[*begin(below)] = false;
+
+    cout << count(begin(can_be_removed), end(can_be_removed), true) << '\n';
+
+    auto cnt = 0;
+    for (auto i = 0; i < n; ++i)
+        if (!can_be_removed[i])
+            cnt += count_dependant(below_vec, above_vec, i);
+    cout << cnt << '\n';
+}
+
+int main()
+{
+    auto n = 0;
+    vector<Seg> v;
+
+    string s;
+    while (getline(cin, s)) {
+        string_view sv{s};
+
+        auto sep = sv.find('~');
+        auto src = split(sv.substr(0, sep), ',');
+        auto dst = split(sv.substr(sep + 1), ',');
+
+        Point from{str2num(src[2]), str2num(src[0]), str2num(src[1])};
+        Point to{str2num(dst[2]), str2num(dst[0]), str2num(dst[1])};
+        if (from > to)
+            swap(from, to);
+
+        v.emplace_back(from, to, n++);
+    }
+
+    solve(n, v);
+
+    return 0;
+}
+
