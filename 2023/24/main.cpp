8ff3e7d154b1d94d8ffbcb598078e307d0141844
Adrian Stanciu <adrian.stanciu.pub@gmail.com>
2023-12-30 18:58:04 +0200

    2023/24: replace Python solution for 2nd part with C++ solution

--- 2023/24/main.cpp
+++ 2023/24/main.cpp
@@ -26,10 +26,11 @@ struct Point {
     Point(int128 x, int128 y, int128 z, int128 vx, int128 vy, int128 vz)
     : x{x}, y{y}, z{z}, vx{vx}, vy{vy}, vz{vz}
     {}
+
+    auto operator<=>(const Point&) const = default;
 };
 
-auto intersect_inside_rectangle_in_future(const Point& p1, const Point& p2,
-    long min_coord, long max_coord)
+auto intersect(const Point& p1, const Point& p2)
 {
     // (x - x1) / (x2 - x1) = (y - y1) / (y2 - y1)
     // (x - x1) / vx = (y - y1) / vy
@@ -56,32 +57,98 @@ auto intersect_inside_rectangle_in_future(const Point& p1, const Point& p2,
 
     auto y_den = a2 * b1 - a1 * b2;
     if (y_den == 0)
-        return false;
+        return make_tuple(false, 0.0l, 0.0l);
 
     auto y = 1.0l * (c1 * a2 - c2 * a1) / y_den;
     auto x = (c1 - b1 * y) / a1;
 
-    if (x < min_coord || x > max_coord || y < min_coord || y > max_coord )
-        return false;
-
-    auto t1 = (x - p1.x) / p1.vx;
-    auto t2 = (x - p2.x) / p2.vx;
-
-    return t1 > 0 && t2 > 0;
+    return make_tuple(true, x, y);
 }
 
-auto solve(const vector<Point>& v, long min_coord, long max_coord)
+auto solve1(int n, const vector<Point>& v, long min_coord, long max_coord)
 {
-    auto n = ssize(v);
-
     auto cnt = 0;
 
     for (auto i = 0; i < n; ++i)
-        for (auto j = i + 1; j < n; ++j)
-            if (intersect_inside_rectangle_in_future(v[i], v[j], min_coord, max_coord))
+        for (auto j = i + 1; j < n; ++j) {
+            auto [intersected, x, y] = intersect(v[i], v[j]);
+            if (!intersected)
+                continue;
+
+            if (x < min_coord || x > max_coord || y < min_coord || y > max_coord )
+                continue;
+
+            auto t1 = (x - v[i].x) / v[i].vx;
+            auto t2 = (x - v[j].x) / v[j].vx;
+            if (t1 > 0 && t2 > 0)
                 ++cnt;
+        }
 
     return cnt;
+}
+
+long solve2(int n, vector<Point>& v)
+{
+    auto min_vx = min_element(begin(v), end(v), [](const auto& lhs, const auto& rhs) {
+        return tie(lhs.vx, lhs) < tie(rhs.vx, rhs);
+    })->vx;
+    auto max_vx = max_element(begin(v), end(v), [](const auto& lhs, const auto& rhs) {
+        return tie(lhs.vx, lhs) < tie(rhs.vx, rhs);
+    })->vx;
+    auto min_vy = min_element(begin(v), end(v), [](const auto& lhs, const auto& rhs) {
+        return tie(lhs.vy, lhs) < tie(rhs.vy, rhs);
+    })->vy;
+    auto max_vy = max_element(begin(v), end(v), [](const auto& lhs, const auto& rhs) {
+        return tie(lhs.vy, lhs) < tie(rhs.vy, rhs);
+    })->vy;
+
+    auto eq = [](auto value, auto expected) {
+        return abs(value - expected) < 0.001;
+    };
+
+    for (auto vx = min_vx; vx <= max_vx; ++vx)
+        for (auto vy = min_vy; vy <= max_vy; ++vy) {
+            for (auto& p : v) {
+                p.vx -= vx;
+                p.vy -= vy;
+            }
+
+            auto [intersected, x_ref, y_ref] = intersect(v[0], v[1]);
+            if (intersected) {
+                auto ok = true;
+
+                for (auto j = 2; j < n; ++j) {
+                    auto [intersected, x, y] = intersect(v[0], v[j]);
+                    if (!intersected || !eq(x, x_ref) || !eq(y, y_ref)) {
+                        ok = false;
+                        break;
+                    }
+                }
+
+                if (ok) {
+                    auto t0 = (x_ref - v[0].x) / v[0].vx;
+                    auto t1 = (x_ref - v[1].x) / v[1].vx;
+
+                    auto z0 = v[0].z + v[0].vz * t0;
+                    auto z1 = v[1].z + v[1].vz * t1;
+
+                    // z0 = z + vz * t0 | * t1
+                    // z1 = z + vz * t1 | * -t0
+                    // z0 * t1 - z1 * t0 = z * (t1 - t0)
+                    // z = (z0 * t1 - z1 * t0) / (t1 - t0)
+                    auto z = (z0 * t1 - z1 * t0) / (t1 - t0);
+
+                    return x_ref + y_ref + z;
+                }
+            }
+
+            for (auto& p : v) {
+                p.vx += vx;
+                p.vy += vy;
+            }
+        }
+
+    assert(false);
 }
 
 int main()
@@ -106,7 +173,10 @@ int main()
         v.emplace_back(x, y, z, vx, vy, vz);
     }
 
-    cout << solve(v, 200000000000000l, 400000000000000l) << '\n';
+    auto n = ssize(v);
+
+    cout << solve1(n, v, 200000000000000l, 400000000000000l) << '\n';
+    cout << solve2(n, v) << '\n';
 
     return 0;
 }
--- 2023/README.md
+++ 2023/README.md
@@ -1,10 +1,9 @@
-### C++ / Python solutions for [Advent of Code 2023](https://adventofcode.com/2023)
+### C++ solutions for [Advent of Code 2023](https://adventofcode.com/2023)
 
 #### Prerequisites
 * 64-bit Linux
 * g++ with c++20 support
 * make
-* python3 with z3-solver
 
 #### Compile
     make build
